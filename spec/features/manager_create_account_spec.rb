772e9feb924872420d60365e9f5da879243045c6
Gregory Sheppard <gregory.sheppard@live.com>
2014-04-03 13:31:29 -0400

    Completed User creation tests

--- spec/features/manager_create_account_spec.rb
+++ spec/features/manager_create_account_spec.rb
@@ -7,12 +7,10 @@ feature 'manager create account', %q{
 } do
 
   # Acceptance Criteria
-  # - I must enter a unique username
   # - I must enter a valid unique email
   # - I must enter a password
   # - I must confirm my password
   # - If I do not enter all required information, I receive an error
-  # - I receive an error message if my username is not unique
   # - I receive an error message if I provided an invalid email
   # - I receive an error message if my email is not unique
   # - I receive an error message if my password does not match up
@@ -32,13 +30,38 @@ feature 'manager create account', %q{
     fill_in 'Email', with: user.email
     fill_in 'user_password', with: user.password
     fill_in 'user_password_confirmation', with: user.password
-    save_and_open_page
+
     click_button 'Create'
 
     expect(page).to have_content('Account successfully created.')
     expect(page).to have_content('Sign Out')
   end
 
-  scenario 'attempt to create account with invalid attributes'
+  scenario 'attempt to create account with invalid attributes' do
+    visit new_user_registration_path
+
+    click_button 'Create'
+
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+  scenario 'ensure email uniqueness' do
+    user_1 = FactoryGirl.create(:user)
+    user_2 = FactoryGirl.build(:user)
+
+    visit new_user_registration_path
+
+    fill_in 'First Name', with: user_2.first_name
+    fill_in 'Last Name', with: user_2.last_name
+    fill_in 'Email', with: user_1.email
+    fill_in 'user_password', with: user_2.password
+    fill_in 'user_password_confirmation', with: user_2.password
+
+    click_button 'Create'
+
+    expect(page).to have_content('has already been taken')
+    expect(page).to_not have_content('Sign Out')
+  end
 
 end
