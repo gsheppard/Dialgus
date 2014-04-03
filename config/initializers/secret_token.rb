7dd948ddae047e5078eb2177c064459656342abf
Gregory Sheppard <gregory.sheppard@live.com>
2014-04-03 11:18:57 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dialgus::Application.config.secret_key_base = 'e1ddde158014e7403f5b1ae8bdcb6262189ea8d578afb4a3723550b73341f456c0c0f06586bacbd7db31019a28512c48dc0e116b72804d8685384d072a763a99'
