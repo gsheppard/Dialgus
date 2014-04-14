65b087e2753530977aeecf2e3c687c7a3409ba7c
Gregory Sheppard <gregory.sheppard@live.com>
2014-04-14 11:11:23 -0400

    Added unit tests for uniquness contraint on Positions Name, scoped to User.

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -3,10 +3,23 @@ require 'spec_helper'
 describe Position do
 
   context 'validations' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:position1) { FactoryGirl.create(:position, user: user) }
+
     it { should validate_presence_of :name }
     it { should validate_presence_of :user }
 
-    it { should validate_uniqueness_of(:name).scoped_to(:user) }
+    it 'should have unique name scoped to user' do
+      position2 = FactoryGirl.build(:position, name: position1.name, user: user)
+
+      expect(position2).to_not be_valid
+    end
+
+    it 'should allow duplicate positions if they belong to separate users' do
+      position2 = FactoryGirl.build(:position, name: position1.name)
+
+      expect(position2).to be_valid
+    end
   end
 
   context 'associations' do
